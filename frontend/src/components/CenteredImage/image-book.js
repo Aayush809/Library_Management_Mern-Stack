80c1fa34a49debf9697ed648f9e7120203ee379b
Aayush809 <120305628+Aayush809@users.noreply.github.com>
2023-12-16 18:44:11 +0530

    Update image-book.js

--- frontend/src/components/CenteredImage/image-book.js
+++ frontend/src/components/CenteredImage/image-book.js
@@ -5,7 +5,7 @@ const FullWidthImage = () => {
   return (
     <div className="full-width-container">
       <img
-        src="https://images.pexels.com/photos/2946979/pexels-photo-2946979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        src="https://images.pexels.com/photos/6334916/pexels-photo-6334916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         alt=""
         className="full-width-image"
       />
